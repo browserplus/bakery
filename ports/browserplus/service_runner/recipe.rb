e2884300af5fd032cd6fd20907564a89dc392bb8
Gary MacDonald <garymd@yahoo-inc.com>
2010-10-06 13:45:47 -0700

    bump servicerunner to 2.10.5 for dependent services

--- ports/browserplus/service_runner/recipe.rb
+++ ports/browserplus/service_runner/recipe.rb
@@ -1,12 +1,12 @@
 {
   :url => {
-    :Windows => 'http://github.com/downloads/browserplus/platform/bpsdk_2.10.2-Win32-i386.zip',
-    :MacOSX => 'http://github.com/downloads/browserplus/platform/bpsdk_2.10.2-Darwin-i386.tgz',
+    :Windows => 'http://github.com/downloads/browserplus/platform/bpsdk_2.10.5-Win32-i386.zip',
+    :MacOSX => 'http://github.com/downloads/browserplus/platform/bpsdk_2.10.5-Darwin-i386.tgz',
     :Linux => 'http://github.com/downloads/browserplus/platform/bpsdk_2.10.2-Linux-x86_64.tgz'
   },
   :md5 => {
-    :Windows => 'afc10e3ca6a1e1fb41cb7d222e0c5b7e',
-    :MacOSX => '6150b35e203e3c9e0d6f3bf3c7c1a5b2',
+    :Windows => 'ab7b672753e7af122c050cf8b1e2ddde',
+    :MacOSX => 'bd8d76e0ad5a3f1ed55448fa44caa5b1',
     :Linux => 'f51e373781c2d4fe93f9cf4bb4b9bd68'
   },
   :install => lambda { |c|
