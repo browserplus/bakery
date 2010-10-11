54fdc8de9fb55e39bb5447c483bd10b356f66216
Gary MacDonald <garymd@yahoo-inc.com>
2010-10-11 12:53:51 -0700

    bump bpsdk

--- ports/browserplus/service_runner/recipe.rb
+++ ports/browserplus/service_runner/recipe.rb
@@ -1,12 +1,12 @@
 {
   :url => {
-    :Windows => 'http://github.com/downloads/browserplus/platform/bpsdk_2.10.5-Win32-i386.zip',
-    :MacOSX => 'http://github.com/downloads/browserplus/platform/bpsdk_2.10.5-Darwin-i386.tgz',
+    :Windows => 'http://github.com/downloads/browserplus/platform/bpsdk_2.10.6-Win32-i386.zip',
+    :MacOSX => 'http://github.com/downloads/browserplus/platform/bpsdk_2.10.6-Darwin-i386.tgz',
     :Linux => 'http://github.com/downloads/browserplus/platform/bpsdk_2.10.2-Linux-x86_64.tgz'
   },
   :md5 => {
-    :Windows => 'ab7b672753e7af122c050cf8b1e2ddde',
-    :MacOSX => 'bd8d76e0ad5a3f1ed55448fa44caa5b1',
+    :Windows => '1c1bab8616496f80354e44aec8b2a2ad',
+    :MacOSX => 'ce59efb736830339c840704b8c50585d',
     :Linux => 'f51e373781c2d4fe93f9cf4bb4b9bd68'
   },
   :install => lambda { |c|
