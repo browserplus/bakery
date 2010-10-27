0f6b7138042b89a6807b6bb65a5c6630fd28ea25
Gary MacDonald <garymd@yahoo-inc.com>
2010-10-27 11:42:41 -0700

    okay, really fixed bpsdk download this time

--- ports/browserplus/service_runner/recipe.rb
+++ ports/browserplus/service_runner/recipe.rb
@@ -1,12 +1,12 @@
 {
   :url => {
-    :Windows => 'http://github.com/downloads/browserplus/platform/bpsdk_2.10.7-Win32-i386.zip',
-    :MacOSX => 'http://github.com/downloads/browserplus/platform/bpsdk_2.10.7-Darwin-i386.tgz',
+    :Windows => 'http://github.com/downloads/browserplus/platform/bpsdk_2.10.7X-Win32-i386.zip',
+    :MacOSX => 'http://github.com/downloads/browserplus/platform/bpsdk_2.10.7X-Darwin-i386.tgz',
     :Linux => 'http://github.com/downloads/browserplus/platform/bpsdk_2.10.2-Linux-x86_64.tgz'
   },
   :md5 => {
-    :Windows => '8ead2bcd0bc32acd418399803a42c1ad',
-    :MacOSX => 'e5564bf09c130c74667f02a48f1134d4',
+    :Windows => 'a4d7438b5f0df4e0c88305154feebfe3',
+    :MacOSX => 'eef92f8ba4fe248ab4d9fa55147a0586',
     :Linux => 'f51e373781c2d4fe93f9cf4bb4b9bd68'
   },
   :install => lambda { |c|
