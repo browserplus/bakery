b057f903d9d41371d9528adbea75b03e6fea63cb
Gary MacDonald <garymd@yahoo-inc.com>
2010-11-08 15:18:11 -0800

    bump servicerunner to 2.10.8 bpsdk

--- ports/browserplus/service_runner/recipe.rb
+++ ports/browserplus/service_runner/recipe.rb
@@ -1,20 +1,21 @@
 {
   :url => {
-    :Windows => 'http://github.com/downloads/browserplus/platform/bpsdk_2.10.7X-Win32-i386.zip',
-    :MacOSX => 'http://github.com/downloads/browserplus/platform/bpsdk_2.10.7X-Darwin-i386.tgz',
+    :Windows => 'http://github.com/downloads/browserplus/platform/bpsdk_2.10.8-Win32-i386.zip',
+    :MacOSX => 'http://github.com/downloads/browserplus/platform/bpsdk_2.10.8-Darwin-i386.tgz',
     :Linux => 'http://github.com/downloads/browserplus/platform/bpsdk_2.10.2-Linux-x86_64.tgz'
   },
   :md5 => {
-    :Windows => 'a4d7438b5f0df4e0c88305154feebfe3',
-    :MacOSX => '7559ff43d032acb82d9bb394990297ac',
+    :Windows => 'c5fc02d9a11f112c3b18d205a10b5b08',
+    :MacOSX => 'b65ca4794d522f9622a4cab02ba2de8f',
     :Linux => 'f51e373781c2d4fe93f9cf4bb4b9bd68'
   },
   :install => lambda { |c|
     ext = (c[:platform] == :Windows ? ".exe" : "")
-    FileUtils.cp(File.join(c[:src_dir], "bin", "ServiceRunner#{ext}"),
+    int = (c[:platform] == :Windows ? "bpsdk" : "")
+    FileUtils.cp(File.join(c[:src_dir], "#{int}", "bin", "ServiceRunner#{ext}"),
                  c[:output_bin_dir],
                  { :verbose => true, :preserve => true })
-    FileUtils.cp(File.join(c[:src_dir], "bin", "BrowserPlus.crt"),
+    FileUtils.cp(File.join(c[:src_dir], "#{int}", "bin", "BrowserPlus.crt"),
                  c[:output_bin_dir],
                  { :verbose => true, :preserve => true })
   }
