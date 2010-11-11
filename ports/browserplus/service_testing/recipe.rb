66a1220d2aa65bf09d57680b623b1388283b836d
Gary MacDonald <garymd@yahoo-inc.com>
2010-11-11 12:57:04 -0800

    bump service-testing recipe for latest null check

--- ports/browserplus/service_testing/recipe.rb
+++ ports/browserplus/service_testing/recipe.rb
@@ -1,6 +1,6 @@
 {
   :deps => [ "service_runner" ], 
-  :url => 'github://browserplus/service-testing/548be576b59ddb1b47825195f3f94af29deb158a',
+  :url => 'github://browserplus/service-testing/988e4da053a9c6ca45fa19b58f9ad6f3b8714a8c',
   :install => lambda { |c|
     tgtDir = File.join(c[:output_dir], "share", "service_testing")
     FileUtils.mkdir_p(tgtDir)
