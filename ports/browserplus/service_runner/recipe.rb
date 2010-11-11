49e49b0342758ea60a062c4539514fcf714305cc
Gary MacDonald <garymd@yahoo-inc.com>
2010-11-11 13:01:26 -0800

    updated servicerunner

--- ports/browserplus/service_runner/recipe.rb
+++ ports/browserplus/service_runner/recipe.rb
@@ -1,12 +1,12 @@
 {
   :url => {
-    :Windows => 'http://github.com/downloads/browserplus/platform/bpsdk_2.10.8-Win32-i386.zip',
-    :MacOSX => 'http://github.com/downloads/browserplus/platform/bpsdk_2.10.8-Darwin-i386.tgz',
+    :Windows => 'http://github.com/downloads/browserplus/platform/bpsdk_2.10.9-Win32-i386.zip',
+    :MacOSX => 'http://github.com/downloads/browserplus/platform/bpsdk_2.10.9-Darwin-i386.tgz',
     :Linux => 'http://github.com/downloads/browserplus/platform/bpsdk_2.10.2-Linux-x86_64.tgz'
   },
   :md5 => {
-    :Windows => 'c5fc02d9a11f112c3b18d205a10b5b08',
-    :MacOSX => 'b65ca4794d522f9622a4cab02ba2de8f',
+    :Windows => 'a9bf008a33a7a50db3670a958e8ec674',
+    :MacOSX => '4e616bf238b12eb6b0fb2d548581b194',
     :Linux => 'f51e373781c2d4fe93f9cf4bb4b9bd68'
   },
   :install => lambda { |c|
