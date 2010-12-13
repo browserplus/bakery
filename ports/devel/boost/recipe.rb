f31b62eb3d6228674272089edaf1fede452b5597
Gordon Durand <gordon_durand@yahoo.com>
2010-12-13 13:19:17 -0800

    Move to boost 1.45

--- ports/devel/boost/recipe.rb
+++ ports/devel/boost/recipe.rb
@@ -1,29 +1,20 @@
 {
-  :url => "http://downloads.sourceforge.net/boost/boost_1_42_0.tar.bz2",
-  :md5 => "7bf3b4eb841b62ffb0ade2b82218ebe6",
+  :url => "http://downloads.sourceforge.net/boost/boost_1_45_0.tar.bz2",
+  :md5 => "d405c606354789d0426bc07bea617e58",
   :configure => lambda { |c|
-    # for the configure step we'll build bjam
-    bjamSrcPath = File.join(c[:src_dir], "tools", "jam", "src")
-    Dir.chdir(bjamSrcPath) do
-      puts "building bjam..."
-      if c[:platform] == :Windows
-        bjbb = File.join(bjamSrcPath, "build.bat")
-        system(bjbb)
-      else 
-        system("./build.sh")
-      end
-    end
+    # for the configure step we'll run bootstrap, which builds bjam
+    rpToBuildDir = Pathname.new(c[:build_dir]).relative_path_from(Pathname.pwd).to_s
+    baseCmd = c[:platform] == :Windows ? ".\\bootstrap.bat" : "./bootstrap.sh"
+    baseCmd += " --prefix=#{rpToBuildDir} "
+    Dir.chdir(c[:src_dir]) { 
+      puts "running bootstrap"
+      system(baseCmd)
+    }
   },
   :build => lambda { |c|
+    # now build via bjam built by configure phase
     # run the build from the src_dir
     Dir.chdir(c[:src_dir]) { 
-      buildLibs = %w(system filesystem)
-      buildLibs.insert(-1, "regex") if c[:platform] != :Windows
-
-      # now hunt down the built bjam from our configure step
-      bjamSrcPath = File.join("tools", "jam", "src")
-      bjamPath = Dir.glob("#{bjamSrcPath}/bin.*/**/bjam*")[0]
-
       toolset=""
       if c[:platform] == :MacOSX
         toolset="darwin"
@@ -38,7 +29,6 @@
         uconfig.write("<architecture>\"x86\" ")
         uconfig.write("<linkflags>\"#{c[:os_link_flags]}\" ;\n")
         uconfig.close
-        
       elsif c[:platform] == :Windows
         toolset="msvc"
       elsif c[:platform] == :Linux
@@ -48,7 +38,8 @@
       end
 
       # now use bjam to build 
-      baseCmd = "#{bjamPath} toolset=#{toolset} "
+      baseCmd = c[:platform] == :Windows ? ".\\bjam" : "./bjam"
+      baseCmd += " toolset=#{toolset} "
       baseCmd += " link=static threading=multi runtime-link=static"
       if c[:platform] == :Linux
         baseCmd += " cflags=-fPIC cxxflags=-fPIC"
@@ -60,6 +51,8 @@
       end
 
       buildType = c[:build_type].to_s
+      buildLibs = %w(system filesystem)
+      buildLibs.insert(-1, "regex") if c[:platform] != :Windows
       buildLibs.each() do |l|
         puts "building #{buildType} #{l}..."
         buildCmd = baseCmd + " variant=#{buildType} --with-#{l}"
