e2213e9745e7cd64bbda4bb24ff4905a1afa97f2
Gary MacDonald <garymd@yahoo-inc.com>
2011-01-11 12:56:59 -0800

    update curl recipe for windows

--- ports/devel/curl/recipe.rb
+++ ports/devel/curl/recipe.rb
@@ -18,23 +18,16 @@
     }
   },
   :build => {
-    [ :Linux, :MacOSX ] => "make"
-    #[ :Windows ] => lambda { |c|
-    #  Dir.chdir(c[:src_dir]) do
-    #    # this sequence is stolen from Tools\buildbot.bat
-    #    configStr = "#{c[:build_type].to_s.capitalize}"
-    #    system("Tools\\buildbot\\external.bat")
-    #    # Python pulls down the source of its own deps from their svn repo
-    #    # as part of the build process.  So it's too late for the normal
-    #    # patching mechanism we would use.  Instead we do poor-man's version.
-    #    FileUtils.copy(File.join(c[:recipe_dir], "bzip2-1.0.5", "makefile.msc"), File.join(c[:src_dir], "..\\bzip2-1.0.5"))
-    #    ENV['OLD_PATH'] = "#{ENV['PATH']}"
-    #    ENV['PATH'] = "#{ENV['PATH']};#{c[:wintools_dir].gsub('/', '\\')}\\nasmw"
-    #    devenvOut = File.join(c[:log_dir], "devenv_#{c[:build_type]}.txt")
-    #    system("vcbuild /M1 PCbuild\\pcbuild.sln \"#{configStr}|Win32\" > #{devenvOut}")
-    #    ENV['PATH'] = "#{ENV['OLD_PATH']}"
-    #  end
-    #}
+    [ :Linux, :MacOSX ] => "make",
+    [ :Windows ] => lambda { |c|
+      Dir.chdir(c[:src_dir]) do
+        configStr = "#{c[:build_type].to_s.capitalize}"
+        devenvOut1 = File.join(c[:log_dir], "devenv_upgrade_#{c[:build_type]}.txt")
+        devenvOut2 = File.join(c[:log_dir], "devenv_#{c[:build_type]}.txt")
+        system("vcbuild /upgrade /M1 lib\\libcurl.vcproj \"#{configStr}|Win32\" > #{devenvOut1}")
+        system("vcbuild /M1 lib\\libcurl.vcproj \"#{configStr}|Win32\" > #{devenvOut2}")
+      end
+    }
   },
   :install => {
     [ :MacOSX, :Linux ] => lambda { |c|
@@ -44,9 +37,18 @@
       FileUtils.mv(File.join(c[:output_dir], "lib", "libcurl.dylib"), File.join(c[:output_lib_dir]))
       FileUtils.mv(File.join(c[:output_dir], "lib", "libcurl.la"), File.join(c[:output_lib_dir]))
       FileUtils.mv(File.join(c[:output_dir], "lib", "pkgconfig"), File.join(c[:output_lib_dir]))
+    },
+    :Windows => lambda { |c|
+      # install static lib
+      buildType = c[:build_type].to_s
+      libFile = File.join(c[:src_dir], "lib", buildType, "libcurl.lib")
+      puts "installing #{c[:build_type].to_s} static library..."
+      FileUtils.cp(libFile, c[:output_lib_dir], :verbose => true)
+      # install headers
+      puts "installing headers..."
+      Dir.glob(File.join(File.join(c[:src_dir], "include", "curl", "*.h"))).each { |h|
+        FileUtils.cp_r(h, c[:output_inc_dir], :verbose => true)
+      }
     }
-  #  :Windows => lambda { |c|
-  #    FileUtils.cp("mongoose_s.lib", c[:output_lib_dir])
-  #  }
   },
 }
