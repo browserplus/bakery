fe2ec57d36b484bddcc76253573468e7518924ac
Gordon Durand <gordon_durand@yahoo.com>
2011-03-17 16:46:16 -0700

    add script to build all ports

---
+++ example/build_all.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+#
+# This script tries to build all ports within the bakery.
+
+require "../ports/bakery"
+require 'pp'
+
+# find all recipes
+allRecipes = []
+Dir.glob(File.join("..", "ports", "**", "recipe.rb")).each {  |r|
+  d = File.dirname(r)
+  b = File.basename(d)
+  allRecipes.push(b)
+}
+
+# now remove know broken ones
+[ "curl_ssl", "python26", "python31" ].each { |b| 
+  allRecipes.delete(b)
+}
+
+# now remove ones which don't work on a platform 
+if CONFIG['arch'] =~ /mswin|mingw/
+  allRecipes.delete("nodejs")
+end
+
+# build 'em
+$order = {
+  :output_dir => File.join(File.dirname(__FILE__), "all_build")
+  :packages => allRecipes,
+  :verbose => true
+}
+
+b = Bakery.new $order
+b.build
