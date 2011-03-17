b4f92501652eef352ccd61232e4d5b4317c154ab
Gary MacDonald <garymd@yahoo-inc.com>
2011-03-16 18:15:52 -0700

    bump service testing recipe

--- ports/browserplus/service_testing/recipe.rb
+++ ports/browserplus/service_testing/recipe.rb
@@ -1,6 +1,6 @@
 {
   :deps => [ "service_runner" ], 
-  :url => 'github://browserplus/service-testing/143137880a16f6e8deafc8b022d12694abc2c2d4',
+  :url => 'github://browserplus/service-testing/348d7a31dc926bb90c789a480a6097eefebb7cc4',
   :install => lambda { |c|
     tgtDir = File.join(c[:output_dir], "share", "service_testing")
     FileUtils.mkdir_p(tgtDir)
@@ -11,6 +11,9 @@
                  tgtDir,
                  { :verbose => true, :preserve => true })
     FileUtils.cp(File.join(c[:src_dir], "ruby", "ruby18_cppunit_runner.rb"),
+                 tgtDir,
+                 { :verbose => true, :preserve => true })
+    FileUtils.cp(File.join(c[:src_dir], "ruby", "ruby19_cppunit_runner.rb"),
                  tgtDir,
                  { :verbose => true, :preserve => true })
     FileUtils.cp_r(File.join(c[:src_dir], "ruby", "json"),
